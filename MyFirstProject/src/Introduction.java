6f85037092b3b1797043832f47b6e30999de1f17
zachk <zachk@DESKTOP-81MT63G>
2019-04-06 17:53:48 -0700

    MyFirst Project for Computer Programming

---
+++ MyFirstProject/src/Introduction.java
@@ -0,0 +1,12 @@
+// Program will print my name, favorite color and what I would do if I wont the lottery
+public class Introduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("My Name is Zach");
+			System.out.println("My favorite color is blue");
+			System.out.println("If I won the lottery, I would buy the Indianapolis Colts");
+		}
+	}
+
+
